4398bf2fd5446912453edd43eeeed99f0b83539a
Filip Frącz <filip@fracz.net>
2021-02-07 15:02:31 -0600

    Altered midi path for github

--- js/app.js
+++ js/app.js
@@ -2,82 +2,82 @@ let sounds = [
     {
         "name": "Italy",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Italy.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Italy.mid"
     },
     {
         "name": "United States",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_United_States.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_United_States.mid"
     },
     {
         "name": "Germany",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Germany.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Germany.mid"
     },
     {
         "name": "France",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_France.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_France.mid"
     },
     {
         "name": "Israel",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Israel.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Israel.mid"
     },
     {
         "name": "Netherlands",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Netherlands.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Netherlands.mid"
     },
     {
         "name": "Poland",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Poland.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Poland.mid"
     },
     {
         "name": "India",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_India.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_India.mid"
     },
     {
         "name": "Canada",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Canada2.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Canada2.mid"
     },
     {
         "name": "United Kingdom",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_United_Kingdom.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_United_Kingdom.mid"
     },
     {
         "name": "Russia",
         "category": "National Anthem",
-        "file": "/midi/national_anthem_russia.mid"
+        "file": "/sound-quiz/midi/national_anthem_russia.mid"
     },
     {
         "name": "Mexico",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Mexico.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Mexico.mid"
     },
     {
         "name": "Spain",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Spain.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Spain.mid"
     },
     {
         "name": "Taiwan",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Taiwan.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Taiwan.mid"
     },
     {
         "name": "China",
         "category": "National Anthem",
-        "file": "/midi/chinami_-_China.mid"
+        "file": "/sound-quiz/midi/chinami_-_China.mid"
     },
     {
         "name": "Australia",
         "category": "National Anthem",
-        "file": "/midi/National_Anthems_-_Australia.mid"
+        "file": "/sound-quiz/midi/National_Anthems_-_Australia.mid"
     },
 ]
 
